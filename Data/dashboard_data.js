e75e14c95c5fcb92c08245b7b3cfd0ffb45a51f4
Dương Thành Thanh Duy <duongduy12314@gmail.com>
2026-04-16 18:13:40 +0800

    dsdsd

---
+++ Data/dashboard_data.js
@@ -0,0 +1,1 @@
+var global_jobs = 'ALL;Japanese Comtor;Java Developer;Service Desk Support ;Project Manager;Software Test Engineer;Salesforce Engineer'; var global_templates = 'Group/AutumnTemplate;Group/SummerTemplate;Group/WinterTemplate;Individual/SummerTemplate';
